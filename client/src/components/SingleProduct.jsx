43bc8103f4720f4293514b67c67713a52866677a
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-04-12 14:22:21 -0500

    added functionality to fave icon

--- client/src/components/SingleProduct.jsx
+++ client/src/components/SingleProduct.jsx
@@ -1,25 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCT } from '../utils/queries';
+import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const SingleProduct = ({ productId }) => {
+    const [selectedSize, setSelectedSize] = useState('');
+    const [isFavorite, setIsFavorite] = useState(false);
+
     const { loading, error, data, refetch } = useQuery(QUERY_PRODUCT, {
         variables: { id: productId },
     });
-
-    const [selectedSize, setSelectedSize] = React.useState('');
 
     useEffect(() => {
         refetch({ id: productId });
@@ -35,23 +38,40 @@ const SingleProduct = ({ productId }) => {
         setSelectedSize(event.target.value);
     };
 
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+    };
+
     return (
         <Grid container spacing={2} justifyContent="center">
             <Grid item xs={12} sm={6} md={4} lg={3}>
                 <img
                     src={`/images/${product.image}`}
                     alt={product.productName}
-                    style={{ maxWidth: '100%', height: 'auto' }}
+                    style={{ width: '100%', height: 'auto' }}
                 />
                 <Card sx={{ minWidth: 275 }}>
                     <CardContent>
-                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline', marginBottom: '1rem' }}>
-                            <Typography sx={{ fontSize: 18, fontWeight:'bold', color:'black' }} color="text.secondary">
-                                {product.productName}
-                            </Typography>
-                            <Typography variant="body2">
-                                {product.productPrice}
-                            </Typography>
+                        <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                            <div>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                                    {product.productName}
+                                </Typography>
+                                <Typography variant="body2">
+                                    {product.productPrice}
+                                </Typography>
+                            </div>
+                            {isFavorite ? (
+                                <FavoriteIcon
+                                    style={{ color: 'red', marginLeft: 10, marginBottom: 10 }}
+                                    onClick={toggleFavorite}
+                                />
+                            ) : (
+                                <FavoriteBorderIcon
+                                    style={{ color: 'black', marginLeft: 10, marginBottom: 10 }}
+                                    onClick={toggleFavorite}
+                                />
+                            )}
                         </div>
                         <Typography variant="body2" sx={{ mb: 3 }}>
                             {product.description}
@@ -77,11 +97,12 @@ const SingleProduct = ({ productId }) => {
                         <Button component={Link} to={`/product/${product.id}`} size="small">ADD TO CART</Button>
                     </CardActions>
                 </Card>
-
             </Grid>
         </Grid>
     );
 }
 
 export default SingleProduct;
+
+
 
